b87f173351858fc2ee71e2671a1e694e2a4d4842
David Boles <david.e.boles22@gmail.com>
2016-07-17 16:11:05 -0700

    Random mode 1

--- src/space/davidboles/homeiot/handlers/roomlight/RoomLightValueGetHandler.java
+++ src/space/davidboles/homeiot/handlers/roomlight/RoomLightValueGetHandler.java
@@ -1,6 +1,7 @@
 package space.davidboles.homeiot.handlers.roomlight;
 
 import java.io.IOException;
+import java.util.Random;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -11,10 +12,11 @@ public class RoomLightValueGetHandler implements HttpHandler {
 
 	int mode = 1;//Auto control, 0 for full brightness
 	int brightness = 15;//TODO set initial to 100
+	Random random = new Random();
 	
 	@Override
 	public void handle(HttpExchange arg0) throws IOException {
-		if(mode == 0) HandlerFs.respondOKHTML(arg0, Integer.toString(100));
+		if(mode == 0) HandlerFs.respondOKHTML(arg0, Integer.toString(random.nextInt(100)));
 		if(mode == 1) HandlerFs.respondOKHTML(arg0, Integer.toString(brightness));
 	}
 	
